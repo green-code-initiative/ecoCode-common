00b46b5f3370b54c787ad9520f131f0135bce92c
David DE CARVALHO <ddecarvalho@solocal.com>
2023-02-07 22:43:06 +0100

    [ISSUE 1] init checker tool

--- tool_checks/_core.sh
+++ tool_checks/_core.sh
@@ -16,51 +16,101 @@ NC='\033[0m' # No Color
 ### display debug logs if DEBUG enabled
 ### $1 : message to log
 debug() {
-  if [ $DEBUG == 1 ]; then
+  if [[ $DEBUG == 1 ]]; then
     echo -e "${GREY}--- DEBUG --- $1${NC}"
   fi
 }
 
 ### launch requirements checks
 ### $1 : the command to check
-### $2 : the command label to display in messages
-function check_requirement() {
+function check_installation() {
 
   declare -a command="$1"
-  declare -a label="$2"
 
-  debug "*****  Checking $label installation  *****"
-  debug "command for check = \"$command --version 2>/dev/null 1>/dev/null; echo \$?\""
-  declare -a res=`$command --version 2>/dev/null 1>/dev/null; echo $?`
-  if [ $res == 0 ]; then
-    echo -e "Checking $label installation ... [ ${GREEN}OK${NC} ]"
+  declare -a res=`$command 2>/dev/null 1>/dev/null; echo $?`
+  if [[ $res == 0 ]]; then
+    echo -e " - installation [ ${GREEN}OK${NC} ]"
   else
-    echo -e "Checking $label installation ... [ ${RED}NON OK${NC} ] : please ${PINK}check or install $label${NC}"
+    echo -e " - installation [ ${RED}NON OK${NC} ] : please ${PINK}check or install tool${NC}"
   fi
+  debug "command for check = \"$command 2>/dev/null 1>/dev/null; echo \$?\""
 
 }
 
-### launch requirements checks (command, min version and max version)
-### $1 : the command to check
-### $2 : the command label to display in messages
-### $3 : the minimum version to check
-### $4 : the maximum version to check
-function check_requirement_with_version() {
+### launch requirements checks (command and min version)
+### $1 : java class version to check
+### $2 : the minimum java class version
+### $3 : the minimum java version
+function check_version_min_java() {
 
-  declare -a command="$1"
-  declare -a libelle="$2"
-  declare -a version_min="$3"
-  declare -a version_max="$4"
+  declare -a version_class=$1
+  declare -a version_class_min=$2
+  declare -a version_jdk_min=$3
 
-  check_requirement "$1" "$2"
-
-  debug "*****  Checking $libelle MIN version ('$version_min')  *****"
-  debug "command for check = \"$command --version 2>/dev/null 1>/dev/null; echo \$?\""
-  declare -a res=`$command --version 2>/dev/null 1>/dev/null; echo $?`
-  if [ $res == 0 ]; then
-    echo -e "Checking $libelle min version ('$version_min') ... [ ${GREEN}OK${NC} ]"
+  if [[ "$version_class_min" -le "$version_class" ]]; then
+    echo -e " - min version [ ${GREEN}OK${NC} ]"
   else
-    echo -e "Checking $libelle min version ('$version_min') ... [ ${RED}NON OK${NC} ] : please ${PINK}check or install $libelle${NC}"
+    echo -e " - min version [ ${RED}NON OK${NC} ] : please ${PINK}check or install good version ${NC}"
   fi
+
+  echo -e "    (current class version : $version_class)"
+  echo -e "    (min class version : '$JAVA_CLASS_VERSION_MIN' / min jdk version : '$JAVA_VERSION_MIN')"
+
+}
+
+### launch requirements checks (command and max version)
+### $1 : java class version to check
+### $2 : the maximum java class version
+### $3 : the maximum java version
+function check_version_max_java() {
+
+  declare -a version_class=$1
+  declare -a version_class_max=$2
+  declare -a version_jdk_max=$3
+
+  if [[ "$version_class" -le "$version_class_max" ]]; then
+    echo -e " - max version [ ${GREEN}OK${NC} ]"
+  else
+    echo -e " - max version [ ${RED}NON OK${NC} ] : please ${PINK}check or install good version ${NC}"
+  fi
+
+  echo -e "    (current class version : $version_class)"
+  echo -e "    (max class version : '$JAVA_CLASS_VERSION_MAX' / max jdk version : '$JAVA_VERSION_MAX')"
+
+}
+
+### launch requirements checks (command and min version)
+### $1 : the current version
+### $2 : the minimum version to check
+function check_version_min_maven() {
+
+  declare -a version="$1"
+  declare -a version_min="$2"
+
+  if [[ "$version_min" < "$version" || "$version_min" = "$version" ]]; then
+    echo -e " - min version [ ${GREEN}OK${NC} ]"
+  else
+    echo -e " - min version [ ${RED}NON OK${NC} ] : please ${PINK}check or install good version ${NC}"
+  fi
+
+  echo -e "    (current version : $version / min version : '$version_min')"
+
+}
+
+### launch requirements checks (command and max version)
+### $1 : the current version
+### $2 : the maximum version to check
+function check_version_max_maven() {
+
+  declare -a version="$1"
+  declare -a version_max="$2"
+
+  if [[ "$version" < "$version_max" || "$version" = "$version_max" ]]; then
+    echo -e " - max version [ ${GREEN}OK${NC} ]"
+  else
+    echo -e " - max version [ ${RED}NON OK${NC} ] : please ${PINK}check or install good version ${NC}"
+  fi
+
+  echo -e "    (current version : $version / max version : '$version_max')"
 
 }
