7bea6a846a4f2e9b36f5e2938b4677acbe46292c
Daniele Casal <daniele.casal@lloydsbanking.com>
2023-11-28 11:39:27 +0000

    implemented improvements for windows requirements tool

--- tools/check_requirements/_config.sh
+++ tools/check_requirements/_config.sh
@@ -2,16 +2,8 @@
 #    C O N F I G U R A T I O N    (to modify)
 #####################################################
 
-# if tool display DEBUG logs or not
-DEBUG=0
-# DEBUG=1
-
-# Java version
-JAVA_VERSION_MIN=11
-JAVA_CLASS_VERSION_MIN=55
-JAVA_VERSION_MAX=17
-JAVA_CLASS_VERSION_MAX=61
-
-# Maven version
-MAVEN_VERSION_MIN=3.5.2
-MAVEN_VERSION_MAX=3.9.1
+# Import configurations from config.txt
+while IFS="=" read -r key value
+do
+    declare "$key=$value"
+done < config.txt
