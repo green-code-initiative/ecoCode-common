94a7a2d8ea9f14df1e4485ddf06d20bb78a54631
Daniele Casal <danielecas@gmail.com>
2023-12-01 14:47:10 +0000

    addressed issue with reading the last line of config.txt and handling comments in configuration file

--- tools/check_requirements/_config.sh
+++ tools/check_requirements/_config.sh
@@ -1,9 +1,13 @@
-#####################################################
-#    C O N F I G U R A T I O N    (to modify)
-#####################################################
-
 # Import configurations from config.txt
-while IFS="=" read -r key value
-do
+while IFS="=" read -r key value || [ -n "$key" ]; do
+    # Remove inline comments and trim whitespace
+    key=$(echo "$key" | sed 's/\s*#.*//')
+    value=$(echo "$value" | sed 's/\s*#.*//')
+
+    # Skip empty lines
+    if [[ -z $key ]]; then
+        continue
+    fi
+
     declare "$key=$value"
 done < config.txt
